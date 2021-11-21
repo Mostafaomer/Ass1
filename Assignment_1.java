d92bc13957e3636582a6112604bcac360b10cee8
Mostafaomer <87620409+Mostafaomer@users.noreply.github.com>
2021-11-21 20:59:12 +0200

    Add files via upload

---
+++ Assignment_1.java
@@ -0,0 +1,12 @@
+package assignment._1;
+public class Assignment_1 
+{
+    public static void main(String[] args) 
+    {
+    TV v=new TV(); 
+        System.out.println(v.channel);
+        v.channelUp();
+         System.out.println(v.volumeLevel);
+        v.volumeDown();
+   }
+}
